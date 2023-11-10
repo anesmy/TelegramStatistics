5f668f61020477aa9d46303241eff272e7873bf3
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-11-10 16:47:58 +0200

    Add ChatHistoryMapper and auxiliary models

--- src/main/java/com/example/telegramstatistics/models/Message.java
+++ src/main/java/com/example/telegramstatistics/models/Message.java
@@ -1,13 +1,33 @@
 package com.example.telegramstatistics.models;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
 public class Message {
     private long id;
     private String type;
-    private LocalDate date;
-    private String dateUnixtime;
+    private LocalDateTime date;
     private String from;
-    private long from_id;
-    private String text;
+    private String from_id;
+    private String messageText;
+
+    @JsonSetter("date")
+    public void setDateFromString(String dateString) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        date = LocalDateTime.parse(dateString, formatter);
+    }
+
+    @JsonSetter("text_entities")
+    public void setTextFromArray(List<TextEntity> textEntities) {
+        StringBuilder sb = new StringBuilder();
+        textEntities.forEach(e -> sb.append(e.getText()));
+        messageText = sb.toString();
+    }
 }
---
+++ src/main/java/com/example/telegramstatistics/models/TextEntity.java
@@ -0,0 +1,10 @@
+package com.example.telegramstatistics.models;
+
+import lombok.Data;
+
+@Data
+public class TextEntity {
+
+    private String type;
+    private String text;
+}
